adb869b0c6b7754a5d5473d06fc17ffea324c895
peyman <peymanahmadifar@yahoo.com>
2020-02-09 20:19:03 +0330

    simple model,viewset,serializer

---
+++ languages/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Language
+
+
+class LanguageSerializer(serializers.HyperlinkedModelSerializer): #ModelSerializer
+    class Meta:
+        model = Language
+        fields = ('id', 'url', 'name', 'paradigm')
